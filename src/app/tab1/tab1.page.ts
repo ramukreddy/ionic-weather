48e06b037d68e716e9583080365e6bff1f52c4a4
ramukreddy <ramareddy@k12admins-MacBook-Pro-4.local>
2019-01-07 15:30:55 -0500

    mocked up ui

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { IconMapService } from '../services/icon-map/icon-map.service';
+import { Weather } from '../models/weather';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class CurrentWeatherPage {}
+export class CurrentWeatherPage {
+  currentWeather: Weather = {
+    temperature: 302,
+    condition: 200
+  };
+
+  constructor(public iconMap: IconMapService) { }
+}
